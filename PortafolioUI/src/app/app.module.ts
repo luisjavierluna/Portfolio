bf371b7d7b5f651c29ead766fff965874af1380e
luisjavierluna <javierlunaluis@gmail.com>
2022-09-08 18:23:38 -0500

    Added home section and relocated about section

--- PortafolioUI/src/app/app.module.ts
+++ PortafolioUI/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
