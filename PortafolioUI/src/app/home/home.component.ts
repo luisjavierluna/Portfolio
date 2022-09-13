a14e23c51ea216d05f846dfd295bfb538c215fd2
luisjavierluna <javierlunaluis@gmail.com>
2022-09-13 15:21:33 -0500

    Hidden contact information from the home section

--- PortafolioUI/src/app/home/home.component.ts
+++ PortafolioUI/src/app/home/home.component.ts
@@ -5,11 +5,28 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent{
+  emailInSight?: boolean = false;
+  showHideText?: string = 'Show Email';
+
+  numberInSight?: boolean = false;
 
   constructor() { }
 
-  ngOnInit(): void {
+  showEmail():boolean{
+    if(this.emailInSight){
+      return this.emailInSight = false;
+    }else{
+      return this.emailInSight = true;
+    }
+  }
+
+  showNumber():boolean{
+    if(this.numberInSight){
+      return this.numberInSight = false;
+    }else{
+      return this.numberInSight = true;
+    }
   }
 
 }
