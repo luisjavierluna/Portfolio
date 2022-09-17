88971e508aef939ab1cf434489de78f9e9c46e86
luisjavierluna <javierlunaluis@gmail.com>
2022-09-17 18:54:16 -0500

    Inproved contact form using reactive forms: PART 1

--- PortafolioUI/src/app/app.module.ts
+++ PortafolioUI/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
@@ -26,7 +26,7 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- PortafolioUI/src/app/contact/contact.component.ts
+++ PortafolioUI/src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ContactFormService } from '../contact-form.service';
 import { ContactForm } from '../models/ContactForm';
 
@@ -7,8 +8,11 @@ import { ContactForm } from '../models/ContactForm';
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
-
+export class ContactComponent implements OnInit {
+  constructor(private contactFormService: ContactFormService, private formBuilder: FormBuilder) { }
+  
+  form: FormGroup = this.formBuilder.group({})
+  
   contactForm: ContactForm = {
     id: '',
     name: '',
@@ -16,22 +20,38 @@ export class ContactComponent {
     message: '',
     contactDay: ''
   }
-
-  constructor(private contactFormService: ContactFormService) { }
-
+  
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      name: ['', {
+        validators: [Validators.required]
+      }],
+      email: ['', {
+        validators: [Validators.required]
+      }],
+      message: ['', {
+        validators: [Validators.required]
+      }]
+    })
+  }
+  
   onSubmit(){
-    this.contactFormService.addContactForm(this.contactForm)
-    .subscribe(
-      response => {
-        this.contactForm = {
-          id: '',
-          name: '',
-          email: '',
-          message: '',
-          contactDay: ''
-        }
-      }
-    )
+    console.log(this.form);
+    
+    
+    
+    // this.contactFormService.addContactForm(this.contactForm)
+    // .subscribe(
+    //   response => {
+    //     this.contactForm = {
+    //       id: '',
+    //       name: '',
+    //       email: '',
+    //       message: '',
+    //       contactDay: ''
+    //     }
+    //   }
+    // )
   }
 
 }
