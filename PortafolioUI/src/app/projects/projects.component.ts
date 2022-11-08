f511c47f02ce56bbad49bae9c0db8466d8f147ad
luisjavierluna <javierlunaluis@gmail.com>
2022-11-08 14:03:27 -0600

    Changed the skills summaries look

--- PortafolioUI/src/app/projects/projects.component.ts
+++ PortafolioUI/src/app/projects/projects.component.ts
@@ -12,42 +12,27 @@ export class ProjectsComponent implements OnInit {
 
   listOfProjects: Project[] = [
     {
-      projectTitle: 'Expense Control',
-      shortDescription: 'Income and expense application made with ASP.NET Core',
-      longDescription: 'Expense Control is a system that allows you to track all the expenses and incomes you have more control to take better decitions according to your current situation',
-      tecnologies: [
-        './assets/images/logos/C_SHARP_logo.png',
-        './assets/images/logos/NET_CORE_logo.png',
-        './assets/images/logos/SQL_logo.png',
-        './assets/images/logos/HTML_logo.png',
-        './assets/images/logos/CSS_logo.png',
-        './assets/images/logos/BOOTSTRAP_logo.png',
-        './assets/images/logos/JQUERY_logo.png',
-      ],
-      imageURL: './assets/images/ExpenseControlScreenshoot.png',
-      link: 'https://github.com/luisjavierluna',
-      githublink: 'https://github.com/luisjavierluna/ExpenseControl_ASP.NET'
-    },
-    {
-      projectTitle: 'Movies',
-      shortDescription: 'App made with Angular and Server made with ASP.NET core',
-      longDescription: 'Movies is a web site that shows the current and future most important movie releases',
+      projectTitle: 'Infinite',
+      shortDescription: 'Computer and technology store website made with ASP.NET and Angular',
+      longDescription: 'API made with ASP.NET and UI made with Angular. I used EF Core as ORM for the connection to the database, Identity and Route Guards for the user system, authentication and authorization and the MVC architectural pattern.',
       tecnologies: [
         './assets/images/logos/C_SHARP_logo.png',
         './assets/images/logos/NET_CORE_logo.png',
         './assets/images/logos/SQL_logo.png',
         './assets/images/logos/Angular_logo.png',
         './assets/images/logos/Typescript_logo.png',
+        './assets/images/logos/HTML_logo.png',
+        './assets/images/logos/CSS_logo.png',
         './assets/images/logos/BOOTSTRAP_logo.png',
       ],
-      imageURL: './assets/images/MoviesScreenshoot.png',
-      link: 'https://github.com/luisjavierluna',
-      githublink: 'https://github.com/luisjavierluna/Movies'
+      imageURL: './assets/images/Infinite.png',
+      link: '',
+      githublink: 'https://github.com/luisjavierluna/Mall'
     },
     {
       projectTitle: 'Portfolio',
-      shortDescription: 'Web page made with Angular and ASP.NET Core',
-      longDescription: 'Portfolio is my personal web page where I show the project I am working on and those I worked on',
+      shortDescription: 'Basic resume made mostly with Angular',
+      longDescription: 'Web application made almost entirely with Angular. The backend elements were removed to make it a static website and easier to navigate.',
       tecnologies: [
         './assets/images/logos/Angular_logo.png',
         './assets/images/logos/Typescript_logo.png',
@@ -61,7 +46,41 @@ export class ProjectsComponent implements OnInit {
       imageURL: './assets/images/PortfolioScreenshoot.png',
       link: 'https://luisjavierluna.github.io/Portfolio/',
       githublink: 'https://github.com/luisjavierluna/Portfolio'
-    }
+    },
+    {
+      projectTitle: 'Expense Control',
+      shortDescription: 'App to track income and expenses made with ASP.NET',
+      longDescription: 'Web application, made entirely with ASP.NET. I used Dapper as ORM for the connection to the database, Identity for the user system, authentication and authorization and the MVC architectural pattern.',
+      tecnologies: [
+        './assets/images/logos/C_SHARP_logo.png',
+        './assets/images/logos/NET_CORE_logo.png',
+        './assets/images/logos/SQL_logo.png',
+        './assets/images/logos/HTML_logo.png',
+        './assets/images/logos/CSS_logo.png',
+        './assets/images/logos/BOOTSTRAP_logo.png',
+        './assets/images/logos/JQUERY_logo.png',
+      ],
+      imageURL: './assets/images/ExpenseControlScreenshoot.png',
+      link: '',
+      githublink: 'https://github.com/luisjavierluna/ExpenseControl_ASP.NET'
+    },
+    {
+      projectTitle: 'Movies',
+      shortDescription: 'Movie Theater Website: Website showing current and upcoming releases made with ASP.NET and angular',
+      longDescription: 'API made with ASP.NET and UI made with Angular. I used EF Core as ORM, Identity and Route Guards for user system, authentication and authorization, Angular Material, Markdown and Maps with Leafleat.',
+      tecnologies: [
+        './assets/images/logos/C_SHARP_logo.png',
+        './assets/images/logos/NET_CORE_logo.png',
+        './assets/images/logos/SQL_logo.png',
+        './assets/images/logos/Angular_logo.png',
+        './assets/images/logos/Typescript_logo.png',
+        './assets/images/logos/BOOTSTRAP_logo.png',
+      ],
+      imageURL: './assets/images/MoviesScreenshoot.png',
+      link: '',
+      githublink: 'https://github.com/luisjavierluna/Movies'
+    },
+    
   ]
 
 
